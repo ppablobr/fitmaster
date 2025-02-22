a1e87bc899f76630f75ccc3aac412551b3eeffbe
Pedro Pablo Oliveira <ppablobr@gmail.com>
2025-02-21 22:55:04 -0300

    Initial commit from your app

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -184,7 +184,7 @@ import React from 'react'
                 <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
                   <div>
                     <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-red-100">
-                      <AlertTriangle className="h-6 w-6 text-red-600" aria-hidden="true" />
+                      <LogOut className="h-6 w-6 text-red-600" aria-hidden="true" />
                     </div>
                     <div className="mt-3 text-center sm:mt-5">
                       <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
